e1ea87ffd8ab8c14903bd73d96a276ef5aa26842
Zenith Kandel <kandelze123@gmail.com>
2026-01-05 21:16:41 +0545

    Add initial index.php file with welcome message and instructions for photo transfer

---
+++ index.php
@@ -0,0 +1,12 @@
+Welcome to Photo Transfer
+=======================
+Enter a unique code to transfer your photos
+then upload the photos you want to share.
+then click upload
+you will have an option to delete the photos from the server immediately
+===============================
+the reciever in their side enters the same code
+and previews the photos in the attachment
+and downloads the required ones or all of them
+you will have an option to delete the photos from the server immediately
+===============================
